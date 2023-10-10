916dceeda4f6346dbd8598b78708c094d695b529
NabilCollison-Cofie <108697431+NabilCollison-Cofie@users.noreply.github.com>
2023-10-09 21:22:59 -0400

    Updates

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
 import discord
 import config
+from discord.ext import commands
+import os
 
 TOKEN = config.bot_token
-client = discord.Client(intents=discord.Intents.default())
+client = commands.Bot(command_prefix ="!", intents=discord.Intents.all())
+
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-client.run(TOKEN)
+@client.tree.command(name ="create_group", description="Adding Usersnames of people in a group")
+async def create_group(interaction: discord.Interaction):
+
+    print(f'Place Holder for Adding Usersnames of people in a group to a list and then storring its a list in MongoDb')
+
+@client.command()
+async def my_groups(ctx):
+    print(f'Place Holder for return the names and the users of groups you are a part of ')
+
+@client.command()
+async def my_schedule(ctx):
+    print(f'Place Holder for return your schedule')
+
+
+@client.command()
+async def add_my_event(ctx):
+    print(f'Place Holder to add event on your schedule')
+    
+@client.command()
+async def add_group_event(ctx):
+    print(f'Place Holder to add event on every member on your groups schedule')
+
+@client.command()
+async def remove_group_event(ctx):
+    print(f'Place Holder to remove event on every member on your groups schedule')
+
+@client.command()
+async def remove_my_event(ctx):
+    print(f'Place Holder to remove event on your schedule')
+
+
+async def main():
+    async with client:
+        await load()
+        await client.run(TOKEN)
+
