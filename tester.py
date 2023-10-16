3e9f6fb58277211d36d601cc81433523e496ff6b
liliantnguyen <72271892+liliantnguyen@users.noreply.github.com>
2023-10-15 21:11:22 -0400

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,1 +1,15 @@
+import unittest
+from groups.py import __init__
+from groups.py import addMember
+from groups.py import addEvent
 
+class initUnitTests(unittest.TestCase):
+    #idk if this is right
+    def test_init_name (self):
+        result = __init__("first", "last")
+        self.assertEqual(result, "first last")
+
+class addMemberUnitTests(unittest.TestCase):
+
+
+class addEventUnitTests(unittest.TestCase):
