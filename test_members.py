36e9c3aad47aff1748a989d495891b01b1a8d487
SamXV <samancio8851@gmail.com>
2023-12-03 21:04:14 -0500

    Update test_members.py

--- test_members.py
+++ test_members.py
@@ -26,3 +26,13 @@ def test_check_groups():
     member1.addGroup(group2)
     assert member1.get_member_info() == "Member Name: " + member1.name + "\nBelongs to Groups: " + group1.name + ", " + group2.name
 
+#Still being worked on
+def test_get_group_names():
+    member1 = Member("First member")
+    group1 = Group("First group")
+    member1.addGroup(group1)
+    assert member1.get_group_names == [group1.name]
+
+def test_check_string():
+    member1 = Member("First member")
+    assert member1.__str__ == member1.__str__
