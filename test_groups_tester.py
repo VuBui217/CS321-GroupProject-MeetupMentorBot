2b1d03ad793b94f8839f56f294090ca44b50fea5
Aymaan Bokth <aabokth@gmail.com>
2023-12-03 20:44:23 -0500

    Groups Testing done

--- test_groups_tester.py
+++ test_groups_tester.py
@@ -1,55 +1,68 @@
+
 import pytest
 import events
 import members
-import groups
+from groups import Group
 
-def test_add_multiple_members():
-    group1 = groups.Group("test_group")
-    member1 = "Hayder"
-    member2 = "Lily"
-    member3 = "Nabil"
-    group1.addMember(member1)
-    group1.addMember(member2)
-    group1.addMember(member3)
-    assert group1.members == [member1, member2, member3]
-    
 
-def test_add_multiple_events():
-    group1 = groups.Group("test_group")
-    
-    group1.addEvent("Baseball", 10, 15, 15, 30, 60)
-    group1.addEvent("Study Session", 3, 10, 10, 30, 120)
-    group1.addEvent("Exam", 3, 10, 12, 0, 90)
-    
-    bbEvent = events.Event("Baseball")
-    bbEvent.createEvent(10, 15, 15, 30, 60)
-    
-    studySessionEvent = events.Event("Study Session")
-    studySessionEvent.createEvent(3, 10, 10, 30, 120)
-    
-    examEvent = events.Event("Exam")
-    examEvent.createEvent(3, 10, 12, 0, 90)
-    
-    assert group1.events == [bbEvent, studySessionEvent, examEvent]
-    
+# Define test cases
 
-def test_remove_multiple_events():
-    group1 = groups.Group("test_group")
-    
-    group1.addEvent("Baseball", 10, 15, 15, 30, 60)
-    group1.addEvent("Study Session", 3, 10, 10, 30, 120)
-    group1.addEvent("Exam", 3, 10, 12, 0, 90)
-    
-    bbEvent = events.Event("Baseball")
-    bbEvent.createEvent(10, 15, 15, 30, 60)
-    
-    studySessionEvent = events.Event("Study Session")
-    studySessionEvent.createEvent(3, 10, 10, 30, 120)
-    
-    examEvent = events.Event("Exam")
-    examEvent.createEvent(3, 10, 12, 0, 90)
-    
-    group1.removeEvent(bbEvent)
-    group1.removeEvent(examEvent)
-    
-    assert group1.events == [studySessionEvent]
+
+def test_group_initialization():
+    group1 = Group("Test Group")
+    assert group1.name == "Test Group"
+    assert group1.members == []
+    assert group1.events == []
+
+
+def test_add_member():
+    group1 = Group("Test Group")
+    member_name = "John"
+    group1.addMember(member_name)
+    assert group1.members == [member_name]
+
+
+def test_add_event():
+    group1 = Group("Test Group")
+    event_name = "Test Event"
+    group1.addEvent(event_name, year=2023, month=10, day=15,
+                    startHour=15, startMin=30, duration=60)
+    assert len(group1.events) == 1
+    event = group1.events[0]
+    assert event.name == event_name
+
+
+def test_multiple_groups():
+    group1 = Group("Group 1")
+    group2 = Group("Group 2")
+
+    # Check group names
+    assert group1.name == "Group 1"
+    assert group2.name == "Group 2"
+
+    # Ensure groups have different member lists
+    member1_group1 = "Vu"
+    member1_group2 = "VuVu"
+    group1.addMember(member1_group1)
+    group2.addMember(member1_group2)
+    assert group1.members == [member1_group1]
+    assert group2.members == [member1_group2]
+
+
+def test_multiple_events():
+    group1 = Group("Test Group 1")
+
+    event1_name = "Event 1"
+    event2_name = "Event 2"
+
+    group1.addEvent(event1_name, year=2023, month=10, day=15,
+                    startHour=15, startMin=30, duration=60)
+    group1.addEvent(event2_name, year=2023, month=11, day=20,
+                    startHour=18, startMin=0, duration=90)
+
+    # Check the number of events in the group
+    assert len(group1.events) == 2
+
+    # Check event names
+    assert group1.events[0].name == event1_name
+    assert group1.events[1].name == event2_name
