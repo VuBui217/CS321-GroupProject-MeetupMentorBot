fe689276594e81ced0931db97af45c8ec5b3ab65
SamXV <samancio8851@gmail.com>
2023-12-03 20:53:47 -0500

    Update test_members.py

--- test_members.py
+++ test_members.py
@@ -2,6 +2,7 @@ import pytest
 import events
 import members
 from members import Member
+from groups import Group
 
 def test_initialization():
     member1 = Member("First member")
@@ -11,6 +12,17 @@ def test_check_if_no_groups():
     member1 = Member("First member")
     assert member1.get_member_info() == "Member Name: " + member1.name + "\nBelongs to Groups: "
 
+def test_check_group():
+    member1 = Member("First member")
+    group1 = Group("First group")
+    member1.addGroup(group1)
+    assert member1.get_member_info() == "Member Name: " + member1.name + "\nBelongs to Groups: " + group1.name
+
 def test_check_groups():
     member1 = Member("First member")
-    
+    group1 = Group("First group")
+    group2 = Group("Second group")
+    member1.addGroup(group1)
+    member1.addGroup(group2)
+    assert member1.get_member_info() == "Member Name: " + member1.name + "\nBelongs to Groups: " + group1.name + ", " + group2.name
+
