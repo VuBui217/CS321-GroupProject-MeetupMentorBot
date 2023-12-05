28bb63f5592abef34dc9b6d36d1dd137542226ab
NabilCollison-Cofie <108697431+NabilCollison-Cofie@users.noreply.github.com>
2023-12-04 21:06:07 -0500

    For Testing

--- groups.py
+++ groups.py
@@ -1,6 +1,7 @@
 import discord
 import config
 from discord.ext import commands
+from datetime import datetime, timedelta
 import events
 import members
 
@@ -8,15 +9,12 @@ import members
 Class to represents a group in the discord bot. group of people that want to mange their time and tasks.
 
 """
-
-
 class Group:
-
+    
     """
     Construct Group with the group name, group member names and the events.
     @param name The name of the group.
     """
-
     def __init__(self, name) -> None:
         self.name = name
         self.members = []
@@ -26,11 +24,10 @@ class Group:
     This method will add new member to the group.
     @param member (members.Member): the new member that going to be added to the group.
     """
-
     def addMember(self, member: members.Member):
 
         self.members.append(member)
-
+        
     """
     Method to add new event to the group. 
     @param eventName The name of the event.
@@ -41,17 +38,49 @@ class Group:
     @param startMin  The starting minute of the event.
     @param duration The duration of the event in minutes.
     """
-
-    def addEvent(self, eventName, year, month, day, startHour, startMin, duration):
+    def addEvent(self,year, eventName, month, day, startHour, startMin, duration):
         tempEvent = events.Event(eventName, self)
-        tempEvent.createEvent(year, month, day, startHour, startMin, duration)
+        tempEvent.createEvent(year,month, day, startHour, startMin, duration)
         self.events.append(tempEvent)
+
 
     """
     method to remove an event from the group.
     @param event the event thats going to be deleted. 
     """
-
     def removeEvent(self, event):
         if event in self.events:
             self.events.remove(event)
+            
+    def find_free_interval(self, start_datetime,name):
+        # Define the start time and end time for the search interval
+        search_start_time = datetime.combine(start_datetime.date(), datetime.strptime("09:00", "%H:%M").time())
+        search_end_time = datetime.combine(start_datetime.date(), datetime.strptime("21:00", "%H:%M").time())
+
+        current_time = search_start_time
+
+        while current_time < search_end_time:
+            # Check if there's an event in the group at the current time
+            if any(event.start_time <= current_time < event.end_time for event in self.events):
+                current_time += timedelta(minutes=30)
+                continue
+
+            # Check if any member of the group has an event at the current time
+            all_members_free = all(
+                not any(
+                    event.start_time <= current_time < event.end_time
+                    for group in member.groups
+                    for event in group.events
+                )
+                for member in self.members
+            )
+
+            if all_members_free:
+                # Call the method to add a new event to the group
+                self.addEvent(self,current_time.year, name, current_time.month, current_time.day, current_time.hour, current_time.min, 30)
+                return "New Event " + name + "starts at " + current_time.strftime("%I:%M %p")  # Return the formatted time
+
+            current_time += timedelta(minutes=30)
+
+        return None  # If no free interval is found
+
--- main.py
+++ main.py
@@ -1,22 +1,52 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+from datetime import datetime, timedelta
+from discord.ui import View, Button
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import os.path
 import config
 import asyncio
 import events
 import groups
 import members
+import logging
+
 
 '''
 This is the main Class that implements the classes groups, members, and events
 This class also functions as a GUI for the program.
 
 '''
+creds = None
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
+if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json",SCOPES)
+
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+service = build("calendar", "v3", credentials=creds, static_discovery=False)
 TOKEN = config.bot_token
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 all_users = []
 all_groups = []
+selected_date_button = None
+selected_group_button = None
 
 
 """
@@ -154,6 +184,65 @@ async def my_schedule(interaction: discord.Interaction):
     else:
         schedule_info = '\n'.join(schedule)
         await interaction.response.send_message(f'Your schedule:\n{schedule_info}')
+        
+"""
+  Creates a URL that adds an event to Google Calendar.
+
+  Args:
+      event_name: The name of the event.
+      start_datetime: A datetime object representing the start time of the event.
+      end_datetime: (Optional) A datetime object representing the end time of the event (defaults to 30 minutes after start).
+
+  Returns:
+      A URL that, when clicked, adds the event to the user's Google Calendar.
+  """        
+@client.tree.command(name="google_link", description="Which event are you interested in? (Enter event number or name)'")
+async def create_google_calendar_event_url(interaction: discord.Interaction, event_index: int): 
+    user = interaction.user.name
+    mem = None
+    for people in all_users:
+        if people.name == user:
+            mem = people
+            break
+
+    if mem is None:
+        await interaction.response.send_message(f'"{user}" not initialized')
+        return
+
+    schedule = []
+
+    for group in mem.groups:
+        for event in group.events:
+            schedule.append(event)
+
+    
+    
+    
+        
+    event_index-=1 
+    if event_index < 0 or event_index >= len(schedule):
+        await interaction.response.send_message(f"Invalid event selection. Please try again.")
+        return    
+    gevent = schedule[event_index]
+        
+    
+
+    # Base URL for Google Calendar event creation
+    base_url = "https://calendar.google.com/calendar/render?action=edit"
+
+    # Encode event details
+    encoded_name = gevent.name.replace(" ", "%20")
+    start_time_param = f"dates={gevent.start_time.strftime('%Y%m%dT%H%M%SZ')}"
+    end_time_param = f"end={gevent.end_time.strftime('%Y%m%dT%H%M%SZ')}"
+
+
+    # Combine URL parts
+    url = f"{base_url}&text={encoded_name}&{start_time_param}&{end_time_param}"
+
+
+    await interaction.response.send_message(f"Click this link to add the event to your Google Calendar: {url}")
+           
+
 
 """
 This command allows the user to add an event to an existing group, ensuring that the group exists.
@@ -181,6 +270,8 @@ async def new_group_event(interaction: discord.Interaction, group_name: str, eve
    
     group.addEvent(year, event_name, month, day, start_hour,
                           start_minute, duration_minutes)
+    
+    await interaction.response.send_message(f'New Event Created, <@{event_name}>!')
 
 """
 This command allows the user to remove an event from an existing group,
@@ -212,6 +303,105 @@ async def remove_group_event(interaction: discord.Interaction, group_name: str, 
         await interaction.response.send_message(f'Event "{event_name}" removed from group "{group_name}".')
     else:
         await interaction.response.send_message(f'Event "{event_name}" not found in group "{group_name}".')
+  
+  
+  
+async def process_selections(selected_date, selected_group):
+    # Save selected buttons to global variables
+    selected_date_button = datetime.strptime(selected_date, "%Y-%m-%d")
+    
+    
+    for existing_group in all_groups:  # Replace with your list of groups
+        if existing_group.name == selected_group:
+            selected_group_button = existing_group
+            break
+
+        
+class ChooseDateGroupView(View):
+    def __init__(self, user_groups):
+        super().__init__(timeout=60)
+
+        self.selected_date = None
+        self.selected_group = None
+
+        current_date = datetime.now()
+        for i in range(7):
+            date_id = f"date-{current_date.strftime('%Y-%m-%d')}"
+            button = discord.ui.Button(
+                label=f"{current_date.month}-{current_date.day}",
+                style=discord.ButtonStyle.blurple,
+                custom_id=date_id,
+            )
+            self.add_item(button)
+            current_date += timedelta(days=1)
+
+        for group in user_groups:
+            button = discord.ui.Button(
+                label=group.name, style=discord.ButtonStyle.green, custom_id=group.id
+            )
+            self.add_item(button)
+
+    async def interaction_check(self, interaction: discord.Interaction):
+        return interaction.user == interaction.message.author
+
+    async def on_button_click(self, interaction: discord.Interaction, button: Button,):
+        if button.custom_id.startswith("date-"):
+            self.selected_date = button.custom_id
+        else:
+            self.selected_group = button.label
+
+        # Check if both selections are made
+        if self.selected_date and self.selected_group:
+            self.stop()
+            await interaction.message.edit(content="Selections confirmed!")
+            # Process selections here (call another function, etc.)
+            # You can access user and user_groups via `self`
+            await process_selections(self.selected_date, self.selected_group)
+            
+    
+
+       
+"""
+    This command allows the user to create and name 30 min 
+    meeting between 9 and 7.
+
+    @param group_name: The Name of the New Event
+    
+    """
+@client.tree.command(name="schedule_meeting", description="Schedule a 30-minute meeting")
+async def schedule_meeting(interaction: discord.Interaction,event_name: str):
+    user = interaction.user.name # Get the user who invoked the command
+    current_date = datetime.now()
+
+    # Get the user's groups
+    user = None
+    for people in all_users:
+        if people.name == user:
+            mem = people
+            break
+    
+    if mem is None:
+        await interaction.response.send_message(f'"{user}" not initialized')
+        return
+    
+        
+    user_groups = mem.groups
+    view = ChooseDateGroupView(user, user_groups)
+    await interaction.response.send_message("Select a date and a group for a meeting between 9 and 7 :", view=view)
+
+    # Get the selected date and group
+    selected_date = selected_date_button
+    selected_group = selected_group_button
+    
+    selected_date_button = None
+    selected_group_button = None
+
+    # Find a 30-minute interval where all members are free
+
+    if selected_group is not None:
+        # Call a function to find a 30-minute interval in the selected group's schedule
+        free_interval = selected_group.find_free_interval(selected_date_button,event_name)
+    interaction.response.send_message(free_interval)
 
 
 
